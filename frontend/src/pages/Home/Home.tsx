a519bd41b9bc70f907a2205ca45ab048d446fb5a
Hampus Nilsson <15hannils@gmail.com>
2023-02-01 15:12:53 +0100

    Conditional rendering for applicant and recruiter

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,13 +2,10 @@ import { Typography } from '@mui/material'
 import React from 'react'
 import useUser from '../../util/auth'
 import ApplicantHome from './ApplicantHome/ApplicantHome'
+import RecruiterHome from './RecruiterHome/RecruiterHome'
 
 export default function Home() {
   const [user] = useUser()
-  return (
-    <div>
-      <Typography variant="h1">Home</Typography>
-      <ApplicantHome />
-    </div>
-  )
+  if (user === null) return null
+  return user.role === 'recruiter' ? <ApplicantHome /> : <RecruiterHome />
 }
