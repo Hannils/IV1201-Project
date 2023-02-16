f36da334d0ab472a9c1f577781c603ffb3b09c80
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2023-02-16 18:42:09 +0100

    Home page

--- frontend/src/pages/Home/ApplicantHome/ApplicantHome.tsx
+++ frontend/src/pages/Home/ApplicantHome/ApplicantHome.tsx
@@ -1,39 +1,90 @@
-import { Card, CardContent, List, Stack } from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Skeleton,
+  Stack,
+  Typography,
+} from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { Application } from '../../../util/Types'
+import { Application, CompetenceProfile } from '../../../util/Types'
+import {
+  ArrowForwardIosRounded,
+  DateRangeRounded,
+  HandshakeRounded,
+  SchoolRounded,
+} from '@mui/icons-material'
+import { useQuery } from '@tanstack/react-query'
+import api from '../../../api/api'
+import { useAuthedUser } from '../../../components/WithAuth'
 
-const application: Application = {
-  competenceProfile: [
-    //{ competence: 'Good at working', yearsOfExperience: 1 },
-    //{ competence: 'Very good at working', yearsOfExperience: 2 },
-    //{ competence: 'Excellent at working', yearsOfExperience: 3 },
-  ],
-  status: 'unhandled',
-  availability: [{ startDate: new Date('2009-09-09'), endDate: new Date('2009-10-09') }],
-}
+const pages = [
+  {
+    link: '/competences',
+    heading: 'Your competences',
+    text: 'View and update your current competences',
+    Icon: SchoolRounded,
+  },
+  {
+    link: '/availability',
+    heading: 'Availability',
+    text: 'View and update your availability',
+    Icon: DateRangeRounded,
+  },
+  {
+    link: '/applications',
+    heading: 'Applications',
+    text: 'View your Applications',
+    Icon: HandshakeRounded,
+  },
+]
 
 export default function ApplicantHome() {
+  const { firstname } = useAuthedUser()
   return (
-    <Stack direction="column" spacing={3}>
-      <Card>
-        <CardContent>
-          <Link to="/competences">Manage competence profile</Link>
-          <List>
-            {/* <Typography variant="h2">Your application</Typography>
-              <Typography>Your competences</Typography>
-              {application.competenceProfile.map(({ competence, yearsOfExperience }) => (
-                <ListItem key={competence} disableGutters>
-                  <ListItemText
-                    primary={competence}
-                    secondary={`Years of experience: ${yearsOfExperience}`}
-                  />
-                </ListItem>
-              ))} */}
-          </List>
-        </CardContent>
-      </Card>
+    <Stack spacing={5}>
+      <Box>
+        <Typography variant="h1">Welcome {firstname}!</Typography>
+        <Typography>Lorem ipsum dolor sit amet</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'grid',
+          gap: 4,
+          gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
+        }}
+      >
+        {pages.map(({ link, heading, text, Icon }) => (
+          <Card>
+            <CardActionArea component={Link} to={link}>
+              <CardMedia sx={{ p: 2, display: 'grid', placeItems: 'center' }}>
+                <Icon color="primary" sx={{ fontSize: 70 }} />
+              </CardMedia>
+              <CardContent>
+                <Typography variant="h2" gutterBottom>
+                  {heading}
+                </Typography>
+                <Typography>{text}</Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions sx={{ justifyContent: 'flex-end' }}>
+              <Button component={Link} to={link} endIcon={<ArrowForwardIosRounded />}>
+                View page
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Box>
     </Stack>
   )
 }
